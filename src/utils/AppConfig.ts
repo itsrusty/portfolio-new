c3b06fa9e789098a53c3ca4cf87da9e8a846684b
itsrusty <rusty3031@gmail.com>
2024-02-14 16:05:14 -0700

    feat: update app

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Hiram Gabriel',
+  title: 'Hiram Gabriel',
+  description: 'Backend Developer',
+  author: 'Hiram Gabriel',
   locale_region: 'en-us',
-  locale: 'en',
+  locale: 'es',
 };
